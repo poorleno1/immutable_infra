8000a3be39506160f9689d8ac76af02b9a997d83
Poor Leno <jarekole@gmail.com>
2017-04-26 09:07:53 +0200

    thirs commit

---
+++ scripts/compact.ps1
@@ -0,0 +1,32 @@
+$ErrorActionPreference = "Stop"
+
+Write-Host "defragging..."
+if (Test-Command -cmdname 'Optimize-Volume') {
+    Optimize-Volume -DriveLetter C
+    } else {
+    Defrag.exe c: /H
+}
+
+Write-Host "0ing out empty space..."
+$FilePath="c:\zero.tmp"
+$Volume = Get-WmiObject win32_logicaldisk -filter "DeviceID='C:'"
+$ArraySize= 64kb
+$SpaceToLeave= $Volume.Size * 0.05
+$FileSize= $Volume.FreeSpace - $SpacetoLeave
+$ZeroArray= new-object byte[]($ArraySize)
+ 
+$Stream= [io.File]::OpenWrite($FilePath)
+try {
+   $CurFileSize = 0
+    while($CurFileSize -lt $FileSize) {
+        $Stream.Write($ZeroArray,0, $ZeroArray.Length)
+        $CurFileSize +=$ZeroArray.Length
+    }
+}
+finally {
+    if($Stream) {
+        $Stream.Close()
+    }
+}
+
+Del $FilePath

85853b98b101925f580044b2317dc200bfc375a1
Jarosław Olechno <JAREKOLE@statoilfuelretail.com>
2018-01-28 13:33:35 +0100

    Working PS5.1 Installation in Core

--- scripts/install_tools.ps1
+++ scripts/install_tools.ps1
@@ -1,4 +1,4 @@
 mkdir c:\tools\	
-Start-BitsTransfer -Source "https://download.sysinternals.com/files/SysinternalsSuite.zip" -Description C:\tools\
+Start-BitsTransfer -Source "https://download.sysinternals.com/files/SysinternalsSuite.zip" -Destination C:\tools\
 Expand-Archive C:\tools\SysinternalsSuite.zip -DestinationPath C:\tools\
 del c:\tools\SysinternalsSuite.zip

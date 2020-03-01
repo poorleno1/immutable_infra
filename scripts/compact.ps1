46e6a751a14a8e184e46a28f6f2f8ea31a042333
Jarek Olechno <jarekole@gmail.com>
2020-03-01 23:32:42 +0100

    Adding time zone and disable automatic updates

--- scripts/compact.ps1
+++ scripts/compact.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = "Stop"
 . a:/Test-Command.ps1
 
 
-$pause_time=120
+$pause_time = 120
 
 Write-Host "Waiting to finish possible updates for $pause_time seconds."
 Start-Sleep -s $pause_time
@@ -18,15 +18,16 @@ Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase
     "$env:windir\temp\*",
     "$env:windir\winsxs\manifestcache"
 ) | % {
-        if(Test-Path $_) {
-            Write-Host "Removing $_"
-            try {
-              Takeown /d Y /R /f $_
-              Icacls $_ /GRANT:r administrators:F /T /c /q  2>&1 | Out-Null
-              Remove-Item $_ -Recurse -Force | Out-Null 
-            } catch { $global:error.RemoveAt(0) }
+    if (Test-Path $_) {
+        Write-Host "Removing $_"
+        try {
+            Takeown /d Y /R /f $_
+            Icacls $_ /GRANT:r administrators:F /T /c /q  2>&1 | Out-Null
+            Remove-Item $_ -Recurse -Force | Out-Null 
         }
+        catch { $global:error.RemoveAt(0) }
     }
+}
 
 	
 Write-Host "Removing Page File."
@@ -36,28 +37,29 @@ Set-ItemProperty -Path $pageFileMemoryKey -Name PagingFiles -Value ""
 Write-Host "defragging..."
 if (Test-Command -cmdname 'Optimize-Volume') {
     Optimize-Volume -DriveLetter C
-    } else {
+}
+else {
     Defrag.exe c: /H
 }
 
 Write-Host "0ing out empty space..."
-$FilePath="c:\zero.tmp"
+$FilePath = "c:\zero.tmp"
 $Volume = Get-WmiObject win32_logicaldisk -filter "DeviceID='C:'"
-$ArraySize= 64kb
-$SpaceToLeave= $Volume.Size * 0.05
-$FileSize= $Volume.FreeSpace - $SpacetoLeave
-$ZeroArray= new-object byte[]($ArraySize)
+$ArraySize = 64kb
+$SpaceToLeave = $Volume.Size * 0.05
+$FileSize = $Volume.FreeSpace - $SpacetoLeave
+$ZeroArray = new-object byte[]($ArraySize)
  
-$Stream= [io.File]::OpenWrite($FilePath)
+$Stream = [io.File]::OpenWrite($FilePath)
 try {
-   $CurFileSize = 0
-    while($CurFileSize -lt $FileSize) {
-        $Stream.Write($ZeroArray,0, $ZeroArray.Length)
-        $CurFileSize +=$ZeroArray.Length
+    $CurFileSize = 0
+    while ($CurFileSize -lt $FileSize) {
+        $Stream.Write($ZeroArray, 0, $ZeroArray.Length)
+        $CurFileSize += $ZeroArray.Length
     }
 }
 finally {
-    if($Stream) {
+    if ($Stream) {
         $Stream.Close()
     }
 }
@@ -65,3 +67,23 @@ finally {
 Del $FilePath
 
 
+Write-Host "Disable automatic updates" -ForegroundColor Cyan
+$p = Start-Process "C:\Windows\System32\cscript.exe" -ArgumentList "C:\windows\System32\SCregEdit.wsf /AU 1" -Wait -PassThru
+
+Write-Host "exit code is: $($p.ExitCode)"
+
+
+Write-Host "Setting time zone"
+
+ 
+if (!(test-path c:\temp)) { New-Item c:\temp -ItemType Directory } 
+[xml]$XmlDocument = invoke-webrequest -Uri https://raw.githubusercontent.com/poorleno1/systemlocale/master/USRegion.xml -UseBasicParsing | Select-Object -ExpandProperty content
+$XmlDocument.Save("c:\temp\USRegion.xml")
+# Set Locale, language etc.
+& $env:SystemRoot\System32\control.exe "intl.cpl,,/f:`"c:\temp\USRegion.xml`""
+  
+# Set Timezone
+& tzutil /s "Central European Standard Time"
+   
+# Set languages/culture
+Set-Culture en-US

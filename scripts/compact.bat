2509e3686bfaea1585185baaf0acd8a3d53e1393
Jaroslaw Olechno <jaroslaw.olechno@circlekeurope.com>
2021-10-06 08:56:20 +0200

    added some antivirus cleanup

--- scripts/compact.bat
+++ scripts/compact.bat
@@ -1,2 +1,4 @@
+pushd %ProgramFiles%\Windows Defender
+MpCmdRun.exe -RemoveDefinitions -All
 cmd /c %SystemRoot%\System32\reg.exe ADD HKCU\Software\Sysinternals\SDelete /v EulaAccepted /t REG_DWORD /d 1 /f
 cmd /c c:\tools\sdelete.exe -q -z C:

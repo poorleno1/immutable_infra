763c39559207e06ad7f2bd32c91e40483eee2f4c
Jaroslaw Olechno <jaroslaw.olechno@circlekeurope.com>
2021-10-07 09:25:43 +0200

    testing Win 2019 - still OK

--- scripts/compact.ps1
+++ scripts/compact.ps1
@@ -10,26 +10,26 @@ Start-Sleep -s $pause_time
 Write-Host "Cleaning SxS..."
 Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase
 
-@(
-    "$env:localappdata\Nuget",
-    "$env:localappdata\temp\*",
-    "$env:windir\logs",
-    "$env:windir\panther",
-    "$env:windir\temp\*",
-    "$env:windir\winsxs\manifestcache",
-    "$env:TEMP"
-    "c:\TEMP"
-) | % {
-    if (Test-Path $_) {
-        Write-Host "Removing $_"
-        try {
-            Takeown /d Y /R /f $_
-            Icacls $_ /GRANT:r administrators:F /T /c /q  2>&1 | Out-Null
-            Remove-Item $_ -Recurse -Force | Out-Null 
-        }
-        catch { $global:error.RemoveAt(0) }
-    }
-}
+# @(
+#     "$env:localappdata\Nuget",
+#     "$env:localappdata\temp\*",
+#     "$env:windir\logs",
+#     "$env:windir\panther",
+#     "$env:windir\temp\*",
+#     "$env:windir\winsxs\manifestcache",
+#     "$env:TEMP"
+#     "c:\TEMP"
+# ) | % {
+#     if (Test-Path $_) {
+#         Write-Host "Removing $_"
+#         try {
+#             Takeown /d Y /R /f $_
+#             Icacls $_ /GRANT:r administrators:F /T /c /q  2>&1 | Out-Null
+#             Remove-Item $_ -Recurse -Force | Out-Null 
+#         }
+#         catch { $global:error.RemoveAt(0) }
+#     }
+# }
 
 	
 Write-Host "Removing Page File."

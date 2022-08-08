a892b7439638eaa133455614217cbf725f08997b
Jarek Olechno <jarekole@gmail.com>
2022-08-08 13:32:10 +0200

    brand new version and updates

---
+++ scripts/explorer-config.ps1
@@ -0,0 +1,13 @@
+if (!(test-path c:\tools)) {
+    mkdir c:\tools
+}
+
+Start-BitsTransfer "https://raw.githubusercontent.com/poorleno1/ExplorerConfig/main/explorerConfig.ps1" -Destination "c:\tools\ExplorerConfig.ps1"
+
+$action = New-ScheduledTaskAction -Execute "C:\windows\system32\WindowsPowerShell\v1.0\powershell.exe" -Argument "-WindowStyle Hidden -File `"C:\tools\ExplorerConfig.ps1`""
+$trigger = New-ScheduledTaskTrigger -AtLogon
+$trigger.Delay = 'PT3S'
+$principal = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Administrators" -RunLevel Highest
+$settings = New-ScheduledTaskSettingsSet
+$task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -Settings $settings
+Register-ScheduledTask "ExplorerConfig" -InputObject $task

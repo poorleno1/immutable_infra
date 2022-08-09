d1c7b2e74070cbaf5882f84a7ef1f60fd2453f6a
Jarek Olechno <jarekole@gmail.com>
2022-08-09 10:27:33 +0200

    updates to 2019 server gui edition

--- scripts/explorer-config.ps1
+++ scripts/explorer-config.ps1
@@ -2,7 +2,8 @@ if (!(test-path c:\tools)) {
     mkdir c:\tools
 }
 
-Start-BitsTransfer "https://raw.githubusercontent.com/poorleno1/ExplorerConfig/main/explorerConfig.ps1" -Destination "c:\tools\ExplorerConfig.ps1"
+Invoke-WebRequest "https://raw.githubusercontent.com/poorleno1/ExplorerConfig/main/explorerConfig.ps1" -OutFile "c:\tools\ExplorerConfig.ps1"
+#Start-BitsTransfer "https://raw.githubusercontent.com/poorleno1/ExplorerConfig/main/explorerConfig.ps1" -Destination "c:\tools\ExplorerConfig.ps1"
 
 $action = New-ScheduledTaskAction -Execute "C:\windows\system32\WindowsPowerShell\v1.0\powershell.exe" -Argument "-WindowStyle Hidden -File `"C:\tools\ExplorerConfig.ps1`""
 $trigger = New-ScheduledTaskTrigger -AtLogon
